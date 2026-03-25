364e65bb24aa97d7efc6b6df32271363ed2752dc
Sai Ramkumar D <dharajsai_bcs27@mepcoeng.ac.in>
2026-03-25 21:38:47 +0530

    Time: 1312 ms (6.56%), Space: 47.9 MB (99.43%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+       int unique=0;
+        for(int i=0;i<candyType.length;i++)
+        {
+            boolean seen=false;
+            for(int j=0;j<i;j++)
+            {
+                if(candyType[i]==candyType[j] && i!=j)
+                {
+                    seen=true;
+                    break;
+                }
+            }
+            if(!seen) unique++;
+        }
+        return Math.min(unique,candyType.length/2);
+    }
+}
